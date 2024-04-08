84e8be415c14c44eab4565093f4cd7df25f73637
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-08 09:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let targetElement = document.querySelector("#level");
+
+let finalLevel = levelOfDom(level);
+
+function levelOfDom(element){
+	let level = 0;
+	while (element !== document.documentElement) {
+		level++;
+		element = element.parentNode
+	}
+	return level + 1;
+}
+
+alert(finalLevel);
